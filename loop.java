c9b17756b40687d719b303d9392f06fad0db61d3
krushnamistry <127290405+krushnamistry@users.noreply.github.com>
2023-05-29 07:11:06 -0700

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,19 @@
+package first_java;
+
+public class loop {
+
+	public static void main(String[] args)
+	{
+			int i;
+			{
+				for(i=0;i<=20;i++)
+				{
+					if(i%2==0)
+					{  System.out.println(i);  }
+				}
+			}
+	}
+}
+
+
+
